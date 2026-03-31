1e5d1469166a4ab6b0cc6e5dddd03fc642556960
Yasaswi <yasaswilanka@gmail.com>
2026-03-31 14:04:57 +0530

    Add files via upload

---
+++ matrixmulti.py
@@ -0,0 +1,9 @@
+a=[[1,2],[3,4]]
+b=[[5,6],[7,8]]
+result=[[0,0],[0,0]]
+for i in range(len(a)):
+    for j in range(len(b[0])):
+        for k in range(len(b)):
+            result[i][j]+=a[i][k]*b[k][j]
+for r in result:
+    print(r)
